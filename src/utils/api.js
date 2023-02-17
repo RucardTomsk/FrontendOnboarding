e7c27dac0cd184e853d0f544d003cab5104e644f
Rucard <www.carat@list.ru>
2023-02-18 01:10:17 +0700

    add utils/api

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,21 +2,18 @@ import axios from "axios";
 
 const api = axios.create()
 
-//start request
 api.interceptors.request.use(config => {
     if(localStorage.access_token) {
         config.headers.authorization = `Bearer ${localStorage.access_token}`
     }
 
-    //Надо возвращать конфиг после его модификации
+
     return config
 },error => {
-    //Этот блок кода срабатывает только тогда, когда ошибка отправки запроса с фронта
+
     console.log(error)
 })
-//end request
 
-//start response
 api.interceptors.response.use(config => {
     if(localStorage.access_token) {
         config.headers.authorization = `Bearer ${localStorage.access_token}`
@@ -24,6 +21,5 @@ api.interceptors.response.use(config => {
 
     return config
 })
-//end response
 
 export default api
