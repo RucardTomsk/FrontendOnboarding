401785d97d95ed32bde4ba2b920ff02d40230cf8
glekovkirilll <glekovkirill2016@gmail.com>
2023-02-17 23:58:51 +0700

    Components

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "@/router/router"
 import {store} from "./store"
+import components from '@/components'
 
 
-createApp(App)
+const app = createApp(App)
     .use(store)
     .use(router)
     .mount('#app')
+
+components.forEach(component => {
+    app.component(component.name, component)
+})
